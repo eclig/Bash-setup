b73c87ca3fa0a5b4f3444ad4b2f5d92da418e444
Emilio Lopes <emilio.lopes@toptica.com>
2017-10-16 09:55:33 +0200

    add alias to connect to `buildhost2' using `rdesktop'

--- .bash.d/toptica.bash
+++ .bash.d/toptica.bash
@@ -6,3 +6,5 @@ type -t hostname > /dev/null 2>&1 ||
 
 export http_proxy=http://192.168.50.2:3128
 export https_proxy=http://192.168.50.2:3128
+
+alias buildhost2='rdesktop -z -u bh2-admin -g 1280x1024 buildhost2'
